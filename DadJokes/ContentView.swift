2cac13e5a2778cf5abf284af47c7f45ceaff6eaf
KenFai <ngkenfai@gmail.com>
2020-10-25 15:18:53 +0800

    Initial Commit

---
+++ DadJokes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DadJokes
+//
+//  Created by Ginger on 25/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
